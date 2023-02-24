f2e5cdda41ee81eeb9b251f5e3217f533d491b4d
AnAvaragelilmemer <116945939+AnAvaragelilmemer@users.noreply.github.com>
2023-02-24 22:44:20 +0800

    Update homebrew.lua

--- homebrew.lua
+++ homebrew.lua
@@ -1,4 +1,5 @@
 repeat task.wait() until game:IsLoaded() and game.ContentProvider.RequestQueueSize == 0;
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AnAvaragelilmemer/scripts/main/toggler"))()
 local lplr = game.Players.LocalPlayer
 local chlplr = lplr.Character.Humanoid
 local name = lplr.Name
