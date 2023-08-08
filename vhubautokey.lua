0d879b3fabdd06a9b8704238cc7801729da5f0af
xeroa <116945939+AnAvaragelilmemer@users.noreply.github.com>
2023-08-09 05:37:14 +0800

    Create vhubautokey.lua

---
+++ vhubautokey.lua
@@ -0,0 +1,5 @@
+--because am lazy to put vhub's key 
+if game.CoreGui:FindFirstChild("KeyScreen") then game.CoreGui.KeyScreen.KeyFrame.TextBox_Key.Text = ""..loadstring(game:HttpGet("https://github.com/AnAvaragelilmemer/scripts/blob/main/vhubkey.lua?raw=true"))()
+    else
+    print("vhub not detected")
+end
