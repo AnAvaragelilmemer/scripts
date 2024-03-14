3e1e15f96daf2e5d5c92ac8c4adcf1ce1aeb9e29
rtaxevasion <116945939+AnAvaragelilmemer@users.noreply.github.com>
2024-03-14 20:57:16 +0800

    Create antitroll.lua

---
+++ antitroll.lua
@@ -0,0 +1,19 @@
+
+local metatable = getrawmetatable(game)
+local oldnamecall = metatable.__namecall
+local byfrontrolls = {
+    "https://github.com/FilteringEnabled/FE/blob/main/asd?raw=true",
+    "https://raw.githubusercontent.com/FilteringEnabled/FE/main/asd",
+    "https://pastebin.com/raw/YMZmLycm"
+}
+setreadonly(metatable,false)
+metatable.__namecall = function(self,url,...)
+local method = getnamecallmethod()
+if (method == "HttpGet" or method == "HttpGetAsync") and self == game and table.find(byfrontrolls,url) then 
+    print("fuck "..url.." one more byfron troll and am going insane")
+    return nil
+end
+return oldnamecall(self,url,...)
+end
+setreadonly(metatable,true)
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "anti byfron troll loaded",Text = "for those who want to use nameless admin"})
